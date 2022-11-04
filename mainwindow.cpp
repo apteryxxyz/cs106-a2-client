e72b817ca304c8edd01624fb97810fde63deaf1f
Apteryx <apteryx.xyz@gmail.com>
2022-11-05 11:25:49 +1300

    Remove unused include

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,12 +1,10 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-#include <QMessageBox>
-
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
-    , worker(new Worker(parent, "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImFkbWluIiwiaWF0IjoxNjY3NTQxNDkxLCJleHAiOjE2Njc1NDUwOTF9.scpEBVRsrXdu4NcrFx-qTQCHgPhx9fFgbT27uXVPkeY"))
+    , worker(new Worker(parent, ""))
 {
     ui->setupUi(this);
 }
