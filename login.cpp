3bb1792e6d2a49531a2d01fcf90ce1a267146698
Xaephare <100387237+Xaephare@users.noreply.github.com>
2022-11-16 13:35:26 +1300

    Added login on 'enter' pressed functionality

--- login.cpp
+++ login.cpp
@@ -54,3 +54,41 @@ void Login::on_LoginButton_clicked()
     }
 }
 
+void Login::keyPressEvent(QKeyEvent *event)
+{
+    if (event->key() == Qt::Key_Return) {
+        // Create the credentials object
+        QString email_address = ui->EmailInput->text();
+        QString password = ui->PasswordInput->text();
+        QJsonObject credentials;
+        credentials.insert("email_address", email_address);
+        credentials.insert("password", password);
+
+        // Send the login request
+        QJsonDocument login_body;
+        login_body.setObject(credentials);
+        QString login_response = worker->post("/login", login_body.toJson());
+        QJsonDocument login_doc = QJsonDocument::fromJson(login_response.toUtf8());
+        QJsonObject login_object = login_doc.object();
+
+        if (login_object.contains("status")) {
+            // If the login response has the "status" field, then an error ocurred
+            QMessageBox::information(this, "Login Failed", "Incorrect email or password");
+        } else {
+            QString token = login_object.value("token").toString();
+            int type = login_object.value("user_type").toInt();
+
+            if (type == 1) {
+                AdminMenu *menu = new AdminMenu(this);
+                menu->worker->set_token(token);
+                menu->show();
+                this->hide();
+            } else if (type == 2) {
+                // Successful member login, show member window
+                QMessageBox::information(this, "Login Successful", "You are a member!");
+            } else {
+                QMessageBox::information(this, "Unknown Error", "An unknown error has occurred!");
+            }
+    }
+}}
+
--- login.h
+++ login.h
@@ -5,6 +5,7 @@
 
 #include <QMainWindow>
 #include <QMessageBox>
+#include <QKeyEvent>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class Login; }
@@ -22,6 +23,7 @@ public:
 
 private slots:
     void on_LoginButton_clicked();
+    void keyPressEvent(QKeyEvent *event);
 
 private:
     Ui::Login *ui;
